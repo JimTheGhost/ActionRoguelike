bc1236020007319e5e674244e1a4591d475e9fcf
James Hill <63484552+JimTheGhost@users.noreply.github.com>
2026-06-19 22:19:32 -0400

    End lesson 26 cont

--- Source/ActionRoguelike/Private/ARLAnimInstance.cpp
+++ Source/ActionRoguelike/Private/ARLAnimInstance.cpp
@@ -3,3 +3,27 @@
 
 #include "ARLAnimInstance.h"
 
+#include "GameplayTagContainer.h"
+#include "ActionSystem/ARLActionComponent.h"
+
+void UARLAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+	AActor* OwningActor = GetOwningActor();
+	if (OwningActor)
+	{
+		ActionComp = Cast<UARLActionComponent>(OwningActor->GetComponentByClass(UARLActionComponent::StaticClass()));
+	}
+}
+
+void UARLAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+	
+	static FGameplayTag StunnedTag = FGameplayTag::RequestGameplayTag("Status.Stunned");
+	
+	if (ActionComp)
+	{
+		IsStunned = ActionComp->ActiveGameplayTags.HasTag(StunnedTag);
+	}
+}
