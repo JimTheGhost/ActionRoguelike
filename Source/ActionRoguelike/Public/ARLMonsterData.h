b3d5af8bd633d311b09ed397c2cf51d350abf4d9
James Hill <63484552+JimTheGhost@users.noreply.github.com>
2026-06-24 16:30:47 -0400

    Lesson 28 End

---
+++ Source/ActionRoguelike/Public/ARLMonsterData.h
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "ARLMonsterData.generated.h"
+
+class UARLAction;
+class AARLAICharacter;
+/**
+ * 
+ */
+UCLASS()
+class ACTIONROGUELIKE_API UARLMonsterData : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Spawn Attributes")
+	TSubclassOf<AARLAICharacter> MonsterClass;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Spawn Attributes")
+	TArray<TSubclassOf<UARLAction>> Actions;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="UI")
+	UTexture2D* Icon;
+};
